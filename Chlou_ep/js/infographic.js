aba54c258c62fbdc183e9ef13918cc41fc277595
mariarasmussen <73876824+mariarasmussen@users.noreply.github.com>
2021-05-27 11:35:47 +0200

    Add files via upload

---
+++ Chlou_ep/js/infographic.js
@@ -0,0 +1,10 @@
+// When the user clicks on <div>, open the popup
+function eyelashPopup() {
+    var popup = document.getElementById("eyelash-popup");
+    popup.classList.toggle("show");
+}
+
+function eyebrowPopup() {
+    var popup = document.getElementById("eyebrow-popup");
+    popup.classList.toggle("show");
+}
